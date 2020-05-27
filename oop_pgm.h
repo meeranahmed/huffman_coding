d79d2bf4a6fb5b5a1b1a78883c852227591076be
nouran <nouran.youssef99@eng-st.cu.edu.eg>
2020-05-28 00:19:21 +0200

    3

--- oop_pgm.h
+++ oop_pgm.h
@@ -14,6 +14,8 @@ class pgm{
     unsigned int cols;
     unsigned int maxVal;
     std::string format;
+    std::vector< char> grayvalues;
+
   private:
     void setrows(std::ifstream * stream){
         *stream>>rows;
@@ -27,14 +29,13 @@ class pgm{
     void setMaxVal (std::ifstream* stream){
         *stream>>maxVal;
     }
-    std::vector< uint8_t> grayvalues;
 
 public:
 
      unsigned int getmaxval(){
          return maxVal;
      }
-     std::vector<short>::size_type get_num_pixels(){
+     std::vector<char>::size_type get_num_pixels(){
          return cols*rows;
      }
      /*void setgrayvalues(std::vector<short>gv){
@@ -44,37 +45,37 @@ public:
      pgm PGM_READ(const std::string(filepath)){
          pgm read_pgm;
          std::ifstream s;
-        //  std::ifstream s(filepath.c_str(), std::ios::in | std::ios::binary);
          s.open(filepath,std::ios_base::binary);
              if(!s)
              {
-                 std::cout << "not opened"<<std::endl;
+                 //std::cout << "not opened"<<std::endl;
                  return read_pgm;
              }
              setformat(&s);
              setcols(&s);
              setrows(&s);
-             std::cout<< "num of pixels  "<<get_num_pixels()<<std::endl;
+            // std::cout<< "num of pixels  "<<get_num_pixels()<<std::endl;
              setMaxVal(&s);
-             std::cout<< "MAXVAL  "<< getmaxval() <<std::endl;
+            // std::cout<< "MAXVAL  "<< getmaxval() <<std::endl;
              read_pgm.grayvalues.resize(get_num_pixels());
 
 
     // read the elements in the file into a vector
 
-             for(std::vector<uint8_t>::size_type i = 0;i<read_pgm.grayvalues.size();i++){
+             for(std::vector<char>::size_type i = 0;i<read_pgm.grayvalues.size();i++){
                  s>>read_pgm.grayvalues[i];
 
              }
 
-             if(get_num_pixels()!=read_pgm.grayvalues.size()){
+            /* if(get_num_pixels()!=read_pgm.grayvalues.size()){
                  std::cout<<"couldn't read all pixels"<<std::endl;
              }
              else
              {
                  std::cout <<"all has been read"<<std::endl;
-             }
-             std::cout<<read_pgm.grayvalues[6];
+             }*/
+
+            // std::cout<<read_pgm.grayvalues[6];
              return read_pgm;
 
      }
