4a714f900e3368651c955384328151781d3330ea
Mariam Ashraf <mariam.aziz99@eng-st.cu.edu.eg>
2020-06-05 17:18:05 +0200

    huff

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    pgm im = PGM_READ("/home/nouranne/oop_pgm/NORMAL2-IM-1431-0001.pgm");
+    pgm im = PGM_READ(/home/mariam/oop_pgm);
     std::unordered_map<uint8_t,int> Frequency = freq_map (im.pixels_values);
     std::unordered_map<uint8_t,string> huffmanCode = buildhuffmanTree( Frequency);
     std::string string = bit_string(im.pixels_values,huffmanCode);
