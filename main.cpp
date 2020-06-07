b8aee2dc0e5a8fcf22476c5b881c9346511742ab
Mariam Ashraf <mariam.aziz99@eng-st.cu.edu.eg>
2020-06-07 14:49:46 +0200

    decoding2

--- main.cpp
+++ main.cpp
@@ -13,11 +13,13 @@ int main()
 
     pgm im = PGM_READ("/home/mariam/oop_pgm/NORMAL2-IM-1431-0001.pgm");
     std::unordered_map<uint8_t,int> Frequency = freq_map (im.pixels_values);
+    /* 
     std::unordered_map<uint8_t,int> freqcount ;
     for (unsigned int i = 0; i< im.pixels_values.size() ; ++i ){
         freqcount[im.pixels_values[i]] ++ ;
         print_freq_map(freqcount);
     }
+    */
       /*
     std::unordered_map<uint8_t,string> huffmanCode = buildhuffmanTree( Frequency);
     std::string string = bit_string(im.pixels_values,huffmanCode);
